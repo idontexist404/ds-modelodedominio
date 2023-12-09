8edb3557651015ba93203798e57275f581edb7a4
highlander <highlander9200@gmail.com>
2023-12-09 14:56:10 -0300

    Create the predefined entities without establishing relationships between them

---
+++ src/main/java/com/highlander/DesafioModeloDeDominioORM/entities/Categoria.java
@@ -0,0 +1,39 @@
+package com.highlander.DesafioModeloDeDominioORM.entities;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "tb_categorai")
+public class Categoria {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(columnDefinition = "TEXT")
+    private String descricao;
+
+    public Categoria() {
+    }
+
+    public Categoria(Integer id, String descricao) {
+        this.id = id;
+        this.descricao = descricao;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+}
