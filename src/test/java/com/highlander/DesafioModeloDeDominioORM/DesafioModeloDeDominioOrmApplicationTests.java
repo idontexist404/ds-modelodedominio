8be696fb57a1b2361e0c378d913269c6d185451a
highlander <highlander9200@gmail.com>
2023-12-09 14:32:15 -0300

    Initial commit with the needed Spring Boot dependencies for the challenge and the in-memory database configuration

---
+++ src/test/java/com/highlander/DesafioModeloDeDominioORM/DesafioModeloDeDominioOrmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.highlander.DesafioModeloDeDominioORM;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioModeloDeDominioOrmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
